c42fe0de3c1408feaabe7a4a8a3c84a2527796ca
marcelinho21junior <msbcontjr@outlook.com>
2022-01-05 13:59:42 -0300

    commit atuzalido no serviço parou 31 minutos

--- script/script.js
+++ script/script.js
@@ -1,7 +1,10 @@
 const card = document.querySelector('.card'),
 card1 = card.querySelector('.card1'),
-cardtext = card1.querySelector(".Card1DigiteNomeError"),
+cardtext = card1.querySelector(".Card1DigiteNome"),
 cardinput = card1.querySelector('.Card1InputDigiteNome')
+card1Button = card1.querySelector(".card1Button")
+
+var api;
 
 cardinput.addEventListener('keyup', e =>{
     if(e.key = "Enter" && cardinput.value != ""){
@@ -9,6 +12,43 @@ cardinput.addEventListener('keyup', e =>{
     }
 });
 
+card1Button.addEventListener("click", () => {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        alert('seu navegador não suporta API de geolocalização')
+    }
+});
+
+function onSuccess(position){
+    const {latitude,longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${`48e2b78c34f243619dcbfe87d8e567aa`}`
+    fetchData();
+}
+
+function onError(error){
+    cardtext.innerText = error.message;
+    cardtext.classList.add("Card1DigiteNomeError")
+}
+
 function requestApi(city){
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={API key}`
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${`48e2b78c34f243619dcbfe87d8e567aa`}`;
+    fetchData();
+}
+
+function fetchData(){
+    cardtext.innerText = "obtendo detalhes do clima...!"
+    cardtext.classList.add("Card1DigiteNomePendente")
+    fetch(api).then(response => response.json().then(result => weatherDetails(result))); 
+}
+
+function weatherDetails(info){
+    cardtext.classList.replace("Card1DigiteNomePendente","Card1DigiteNomeError");
+    if(info.cod == "404"){
+        cardtext.innerText = `${cardinput.value} não é um nome de cidade válido`
+    }else{
+        cardtext.classList.remove("Card1DigiteNomePendente","Card1DigiteNomeError");
+        card.classList.add()
+        console.log(info)
+    }
 }
